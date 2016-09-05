53bb91ea65d27cb1f4e8b27c568af6ebd277fe5e
Kate <kateryna.purchel@gmail.com>
2016-09-05 21:50:05 +0100

    Update ThereIsNoSpoon.java

--- Medium/There Is No Spoon/ThereIsNoSpoon.java
+++ Medium/There Is No Spoon/ThereIsNoSpoon.java
@@ -1,10 +1,8 @@
-package level2;
-
 import java.util.*;
 import java.io.*;
 import java.math.*;
 
-class APU_Init_Phase {
+class Player {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
